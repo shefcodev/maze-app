eff9360decc04ee8df04bc454a591467b76449d3
shefcodev <sherefjimoh@gmail.com>
2023-06-23 22:38:04 +0100

    created a simple matter js demo to get started

--- app.js
+++ app.js
@@ -4,22 +4,26 @@
 
 const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
+const width = 800;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
    element: document.body,
    engine: engine,
    options: {
-      width: 800,
-      height: 600
+      wireframes: false,
+      width,
+      height
    }
-})
+});
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
 World.add(world, MouseConstraint.create(engine, {
    mouse: Mouse.create(render.canvas)
-}))
+}));
 
 // walls
 
@@ -27,7 +31,7 @@ const walls = [
    Bodies.rectangle(400, 0, 800, 40, {
       isStatic: true
    }),
-   Bodies.rectangle(400, 600, 800, 40, {
+   Bodies.rectangle(400, 600, 800, 40, { 
       isStatic: true
    }),
    Bodies.rectangle(0, 300, 40, 600, {
@@ -40,6 +44,32 @@ const walls = [
 
 World.add(world, walls);
 
-World.add(world, Bodies.rectangle(100, 100, 50, 50, {
-   isStatic: false
-}))
+// random shapes
+
+for (let x = 0; x < 50; x++) {
+   if (Math.random() > 0.5) {
+      World.add(
+         world,
+         Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50, 
+            {
+               isStatic: false,
+               render: {
+                  fillStyle: "green"
+               }
+            }
+         )
+      );
+   } else {
+      World.add(
+         world,
+         Bodies.circle(Math.random() * width, Math.random() * height, 35, 
+            {
+               isStatic: false,
+               render: {
+                  fillStyle: "red"
+               }
+            }
+         ) 
+      );
+   }
+}
